022ae6ea43bea99ccb2cf5f1d9bb1302d26b14e3
Kirti <kirtijhala1110@gmail.com>
2025-06-06 22:18:50 +0530

    remove the unrequired code

--- src/main/java/com/webProject/springboot/SpringbootApplication.java
+++ src/main/java/com/webProject/springboot/SpringbootApplication.java
@@ -3,10 +3,6 @@ package com.webProject.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
