313d8173b524c88545f80e32ddc87f0a54fadc8b
Kirti <kirtijhala1110@gmail.com>
2025-06-23 22:38:16 +0530

    lombok depedency injected

--- src/main/java/com/webProject/springboot/Entity/JournalEntity.java
+++ src/main/java/com/webProject/springboot/Entity/JournalEntity.java
@@ -6,7 +6,26 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+// import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.Value;
+
 @Document
+// @Data => equivalent to all the below annotations
+
+// @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Value
+@Builder
 public class JournalEntity {
 
     @Id
@@ -14,43 +33,4 @@ public class JournalEntity {
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    // public void setId(String id) {
-    //     this.id = id;
-    // }
-
-    public JournalEntity(String title,  String content) {
-        // this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-       public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
- 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
 }
